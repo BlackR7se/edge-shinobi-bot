35855e50d8d62b4b7e194c966135ca39130fc81d
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-27 15:32:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,4 +6,30 @@ client.on('ready', () => {
     console.log('Hello, im Shinobi Bot. I was made by Edge#8251 for moderate some Discord servers. Im online the 24 hours the 7 days of the week!');
 });
 
+client.on('message', msg => {
+  if (msg.guild === null) return;
+  if (msg.author.bot) return;
+
+  if (!msg.content.toLowerCase().startsWith(prefix )) return;
+
+  if (msg.content.toLowerCase().startsWith(prefix + "kick")) {
+    var mem = msg.mentions.members.first();
+    mem.kick().then(() => {
+      msg.channel.send({embed:{
+        title:"The user " + mem.displayName + " has been succefully kicked by " + msg.author.username + "!",
+        description:"[Click me to see the rules](https://pastebin.com/zDEcasda)",
+        url:"https://pastebin.com/zDEcasda",
+        color: 0x3EFF00
+      }})
+    }).catch(e) {
+      msg.channel.send({embed:{
+        title:"Error:",
+        description:"An error occured!",
+        color: 0xFF0000
+      }})
+      
+    });
+  }
+}
+
 client.login(process.env.BOT_TOKEN);
