05af5958888bf3f695a22a0ea82e1b164b46a5f6
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-27 15:18:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "s!";
 
 client.on('ready', () => {
-    console.log('Edge was Here!');
+    console.log('Hello, im Shinobi Bot. I was made by Edge#8251 for moderate some Discord servers. Im online the 24 hours the 7 days of the week!');
 });
 
-client.on('message', message => {
-   if (message.content === 'ping') {
-     message.reply('pong');
+client.on('message', () => {
+  if (message.guild === null) return;
+  if (message.author.bot) return;
+
+  if (!message.content.toLowerCase().startsWith(prefix )) return;
+
+  if (message.content.toLowerCase().startsWith(prefix + "kick")) {
+    var mem = message.mentions.members.first();
+    mem.kick().then(() => {
+      message.channel.send({embed:{
+        title:"The user " + mem.displayName + " has been succefully kicked by " + message.author.username + "!",
+        description:"[Click me to see the rules](https://pastebin.com/zDEcasda)",
+        url:"https://pastebin.com/zDEcasda",
+        color: 0x3EFF00
+      }})
+    }).catch(e) {
+      message.channel.send({embed:{
+        title:"Error:",
+        description:"An error occured!",
+        color: 0xFF0000
+      }})
+      
+    );
     }
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
