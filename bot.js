4bd689edcc8e54b9f5af7628f5663485876ad19c
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-27 14:45:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-   if (message.content === 'PING') {
+   if (message.content === 'ping') {
      message.reply('pong');
     }
 });
