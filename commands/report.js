90fae853e4a6c784af4b9abf79396014b2f29456
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-28 13:56:50 -0500

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+     if(args[0] == "help"){
+     message.reply("Usage: s!report <user> <reason>");
+     return;
+    }
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Couldn't find user.");
     let rreason = args.join(" ").slice(22);
