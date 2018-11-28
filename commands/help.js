c28787cbbd922c88273d07a60016ad71ac25d71e
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-28 13:48:11 -0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let helpembed = new Discord.RichEmbed()
+  .setDescription("Help Menu")
+  .setColor("#FFEE00")
+  .addField("Member Commands", "help, serverinfo, botinfo, userinfo, and report.");
+
+  message.channel.send(helpembed);
+  if(message.member.hasPermissions("MANAGE_MESSAGES")){
+  let modembed = new Discord.RichEmbed()
+  .setDescription("HR Help Menu")
+  .setColor("#00FFFF")
+  .addField("HR Commands", "addrole, removerole, kick, warn, warnlevel, ban");
+
+  try{
+    await message.author.send(modembed);
+  }catch(e){
+    message.reply("Your DMs are locked. I cannot send you the HR commands.")
+  }
+}
+
+module.exports.help = {
+  name: "help"
+}
