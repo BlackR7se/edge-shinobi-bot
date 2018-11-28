565226c6e3baa2599f6e6cd8fd7bc967a5ecd8c8
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-28 15:55:17 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,27 +1,23 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("You don't have enough permissions!");
-     if(args[0] == "help"){
-     message.reply("Usage: s!ban <user> <reason>");
-     return;
-    }
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Not enough permissions!");
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be banned!");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
-    .setColor("#FF0000")
+    .setColor("#FFF0000")
     .addField("Banned User", `${bUser} with ID ${bUser.id}`)
     .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
     .addField("Banned In", message.channel)
     .addField("Time", message.createdAt)
     .addField("Reason", bReason);
 
-    let incidentchannel = message.guild.channels.find(`name`, "bans");
-    if(!incidentchannel) return message.channel.send("Can't find bans channel.");
+    let incidentchannel = message.guild.channels.find(`name`, "incidents");
+    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
 
     message.guild.member(bUser).ban(bReason);
     incidentchannel.send(banEmbed);
