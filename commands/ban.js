74e22ce48bef1dc588f2e0a1f8c6d6455f15169d
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-28 13:52:50 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,11 +1,15 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("You don't have enough permissions!");
+     if(args[0] == "help"){
+     message.reply("Usage: s!addrole <user> <role>");
+     return;
+    }
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("You don't have enough permissions!");
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be banned!");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
