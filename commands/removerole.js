00016beb1be2858b85e871aab218555095e25f0b
BlackR7se <45186511+BlackR7se@users.noreply.github.com>
2018-11-28 13:55:12 -0500

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -2,6 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have enough permissions!");
+  if(args[0] == "help"){
+  message.reply("Usage: s!removerole <user> <role>");
+  return;
+ }
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Couldn't find that user.");
   let role = args.join(" ").slice(22);
